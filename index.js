e4cc7ed240f039846eba6577c8a8c1f958058ecc
AndreySuavita <andrey93sam@hotmail.com>
2022-04-26 14:45:58 -0500

    2 files modified

--- index.js
+++ index.js
@@ -5,3 +5,25 @@ import './style.css';
 //const appDiv = document.getElementById('app');
 //appDiv.innerHTML = `<h1>JS Starter</h1>`;
 
+//Gráfico de columnas. Mi 1º gráfico
+
+var visualization = d3plus
+  .viz()
+
+  .container('#viz1')
+
+  .data([
+    { year: 1991, name: 'alpha', value: 15 },
+
+    { year: 1992, name: 'alpha', value: 20 },
+  ])
+
+  .type('bar')
+
+  .id('name')
+
+  .x('year')
+
+  .y('value')
+
+  .draw();
